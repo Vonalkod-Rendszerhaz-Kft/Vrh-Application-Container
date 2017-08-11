ff25a6aa97b2f88dec673819a2892322b607ba8d
Görcsös Zoltán <zgorcsos@vonalkod.hu>
2017-08-11 13:11:07 +0200

    Add new Vrh.Logger version: 1.2.0

---
+++ Vrh.ApplicationContainer.WindowsServiceHost/ApplicationContainerService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vrh.ApplicationContainer.WindowsServiceHost
+{
+    public partial class ApplicationContainerService : ServiceBase
+    {
+        public ApplicationContainerService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if (_applicationContainer != null)
+            {
+                _applicationContainer.Dispose();
+                _applicationContainer = null;
+            }
+            _applicationContainer = new ApplicationContainer();
+        }
+
+        protected override void OnStop()
+        {
+            _applicationContainer.Dispose();    
+        }
+
+        private ApplicationContainer _applicationContainer = null;
+    }
+}
