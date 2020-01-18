3aa60e24b6b0a9e25928c8f412a7461c5956b1f8
Schwirg László <lschwirg@vonalkod.hu>
2020-01-18 18:18:14 +0100

    v4.0.2-noexe - host exe-k nélküli csomag

--- Vrh.ApplicationContainer/Properties/AssemblyInfo.cs
+++ Vrh.ApplicationContainer/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.2.0")]
 [assembly: AssemblyFileVersion("4.0.2.0")]
-[assembly: AssemblyInformationalVersion("4.0.2")]
+[assembly: AssemblyInformationalVersion("4.0.2-noexe")]
